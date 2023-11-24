b5f08bc0e7afa49f6771854796bc22c61d8c1e32
Frank Mai <thxcode0824@gmail.com>
2023-11-24 18:14:35 +0800

    Template generated from Seal

--- modules/aws-vpc/README.md
+++ modules/aws-vpc/README.md
@@ -8,5 +8,8 @@ Provider Requirements:
 * `vpc_id`
 
 ## Managed Resources
+* `aws_subnet.subnet1` from `aws`
+* `aws_subnet.subnet2` from `aws`
+* `aws_subnet.subnet3` from `aws`
 * `aws_vpc.my_vpc` from `aws`
 
--- modules/aws-vpc/main.tf
+++ modules/aws-vpc/main.tf
@@ -1,13 +1,34 @@
+# 1. create aws vpc with 10.0.0.0/8 and name as thxcode-testing;
+
 resource "aws_vpc" "my_vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/8"
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
-
+  
   tags = {
-    Name = "my-vpc"
+    Name = "thxcode-testing"
   }
 }
+
+# 2. create 3 aws subnets under the above created vpc;
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.3.0/24"
+}
+
+# 3. output the id of the vpc created by step 1.
 
 output "vpc_id" {
   value = aws_vpc.my_vpc.id
